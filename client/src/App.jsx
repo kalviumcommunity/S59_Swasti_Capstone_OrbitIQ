1b482fa78c159b66a4d9afa93c08ce5a0f8a2848
Swasti008 <swastimohanty452@gmail.com>
2024-04-01 11:19:16 +0530

    changes made according to Review

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,20 @@
-import Logo from './assets/logo.png'
-import {BarLoader} from 'react-spinners'
-import './App.css'
+import React from "react";
+import Logo from "./assets/logo.png";
+import { BarLoader } from "react-spinners";
+import "./App.css";
 
-function App() {
+const theme = {
+  loaderColor: "#6FFAC9",
+  loaderWidth: "200px",
+};
 
+const App = () => {
   return (
-    <div className='loading'>
-      <img src={Logo} height='200px'/>
-      <BarLoader color="#6FFAC9" width='200px' />
+    <div className="loading">
+      <img src={Logo} height="200px" alt="logo-image" />
+      <BarLoader color={theme.loaderColor} width={theme.loaderWidth} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
