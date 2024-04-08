4a18daa74fcdf932efc2127e16684561ffc48406
Swasti008 <swastimohanty452@gmail.com>
2024-04-08 15:48:51 +0530

    Static Landing Page made

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,18 @@
-import React from "react";
-import Logo from "./assets/logo.png";
-import { BarLoader } from "react-spinners";
+import React, { useState, useEffect } from "react";
+import Loader from "./components/Loader";
+import Landing from "./pages/Landing";
 import "./App.css";
 
-const theme = {
-  loaderColor: "#6FFAC9",
-  loaderWidth: "200px",
-};
-
 const App = () => {
-  return (
-    <div className="loading">
-      <img src={Logo} height="200px" alt="logo-image" />
-      <BarLoader color={theme.loaderColor} width={theme.loaderWidth} />
-    </div>
-  );
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
+  return <>{loading ? <Loader /> : <Landing />}</>;
 };
 
 export default App;
