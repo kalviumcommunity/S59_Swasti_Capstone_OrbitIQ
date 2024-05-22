89bae5bdddf29a82b384ef950bd64b72847e251d
Swasti008 <swastimohanty452@gmail.com>
2024-05-22 17:44:52 +0530

    Get module request added

--- server/src/Routes/learningUnits.routes.js
+++ server/src/Routes/learningUnits.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const Schema = require("../Model/learningUnit_schema")
-const moduleSchema = require("../Model/module_schema")
+const moduleSchema = require("../Model/module_schema");
 
 router.post('/add-lu/:module_id', async (req, res) => {
     const module_id = req.params.module_id
@@ -20,6 +20,16 @@ router.post('/add-lu/:module_id', async (req, res) => {
         console.log(error)
         res.status(500).json({ message: "Failed to add lu. Try again later." })
     }
+})
+router.get('/module', async (req, res) => {
+    try {
+        const modules = await moduleSchema.find().populate('learningUnits');
+        res.status(200).json(modules);
+    }
+    catch (err) {
+        res.status(500).json({ message: "Failed to get modules" })
+    }
+
 })
 
 module.exports = router;
