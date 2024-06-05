a8c53f54db1d1599f5b04ccac755fc7ea946e17c
Swasti008 <swastimohanty452@gmail.com>
2024-06-05 18:58:13 +0530

    Action performed on AI-review

--- server/db.js
+++ server/db.js
@@ -1,14 +1,24 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
 
+//retry functionality
+const connectRetry=()=>{
+  mongoose.connect(process.env.URI, {
+  }).then(()=>{
+    console.log('📦 connected to mongoDB');
+  }).catch((err)=>{
+    console.error('❌ error connecting to mongoDB:', err.message);
+    console.log('Retrying in 5 seconds...');
+    setTimeout(connectRetry, 5000);
+  })
+}
+
 //connecting server to MongoDB
 const connectToDB = async () => {
   try {
-    await mongoose.connect(process.env.URI, {
-    });
-    console.log('📦 connected to mongoDB');
+    await connectRetry();
   } catch (err) {
-    console.error('❌ error connecting to mongoDB:', err.message);
+    console.error('❌ initial connecting to mongoDB phase error:', err.message);
   }
 };
 
--- server/src/Routes/fileUpload.js
+++ server/src/Routes/fileUpload.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { User } = require("../Model/user_schema");
 const multer = require("multer");
+const mongoose=require("mongoose")
 
 const storage = multer.diskStorage({
     destination: function (req, file, nd) {
@@ -17,12 +18,19 @@ const validateFile = (req, file, nd) => {
     }
     nd(null, true);
 }
-const upload = multer({ storage: storage, validateFile: validateFile });
+const limits ={
+    fileSize: 5 * 1024 * 1024
+}
+const upload = multer({ storage: storage, validateFile: validateFile ,limits:limits});
 const path = require('path');
 
 // Database read route performed 
 router.get("/profileImage/:userId", async (req, res) => {
     const userId = req.params.userId;
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+        console.log('Invalid user ID');
+        return res.status(400).send({ error: 'Invalid user ID' });
+    }
     try {
         const user = await User.findById(userId);
         if (!user) {
@@ -40,6 +48,10 @@ router.get("/profileImage/:userId", async (req, res) => {
 router.patch("/updateProfileImage/:userId", upload.single('file'), async (req, res) => {
     try {
         const userId = req.params.userId;
+        if (!mongoose.Types.ObjectId.isValid(userId)) {
+            console.log('Invalid user ID');
+            return res.status(400).send({ error: 'Invalid user ID' });
+        }
         const user = await User.findById(userId);
         if (!user) {
             return res.status(404).send('User not found');
