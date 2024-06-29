d57fc1895836b574df8dba1f7a5cf37fcb457f91
Swasti008 <swastimohanty452@gmail.com>
2024-06-29 20:45:40 +0530

    Used a testing framework JEST in application

---
+++ server/_tests_/authRoutes.test.js
@@ -0,0 +1,104 @@
+const request = require('supertest');
+const app = require('../app');
+const mongoose = require('mongoose');
+const { User } = require("../src/Model/user_schema");
+require('dotenv').config();
+
+beforeAll(async () => {
+    await mongoose.connect(process.env.TEST_URI);
+});
+
+afterAll(async () => {
+    await mongoose.connection.dropDatabase();
+    await mongoose.disconnect();
+});
+
+describe('User Registration', () => {
+    it('should create a new user when valid data is provided', async () => {
+        const response = await request(app)
+            .post('/user/signup')
+            .send({ Username: 'testuser', Email: 'test@example.com', Password: 'testpassword' })
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(201);
+
+        expect(response.body).toHaveProperty('userId');
+        expect(response.body).toHaveProperty('redirectUrl');
+    }, 10000);
+
+    it('should return 400 Bad Request if required fields are missing', async () => {
+        const response = await request(app)
+            .post('/user/signup')
+            .send({ Username: 'testuser' })
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(400);
+
+        expect(response.body).toHaveProperty('error');
+    });
+});
+
+describe('User Login', () => {
+    it('should log in a user with valid credentials', async () => {
+        const response = await request(app)
+            .post('/user/login')
+            .send({ Email: 'test@example.com', Password: 'testpassword' })
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/);
+
+        if (response.status === 200) {
+            except(response.body).toHaveProperty('message', "Login successful")
+            expect(response.body).toHaveProperty('token');
+            expect(response.body).toHaveProperty('Username');
+            expect(response.body).toHaveProperty('Email');
+        } else if (response.status === 401) {
+            expect(response.body).toHaveProperty('message');
+        }
+    });
+
+    it('should return 404 Not Found if user does not exist', async () => {
+        const response = await request(app)
+            .post('/user/login')
+            .send({ Email: 'nonexistent@example.com', Password: 'testpassword' })
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(404);
+
+        expect(response.body).toHaveProperty('message', 'User not found');
+    });
+});
+
+describe('User Deletion', () => {
+    let userIdToDelete;
+
+    beforeEach(async () => {
+        const testUser = await User.create({
+            Username: 'testuser',
+            Email: 'test@example.com',
+            Password: 'testpassword',
+        });
+        userIdToDelete = testUser._id;
+    });
+
+    afterEach(async () => {
+        await User.findByIdAndDelete(userIdToDelete);
+    });
+
+    it('should delete user with valid user ID', async () => {
+        const response = await request(app)
+            .delete(`/user/deleteUser/${userIdToDelete}`)
+            .expect(200);
+
+        expect(response.body).toHaveProperty('message', 'User deleted successfully');
+    });
+
+    it('should return 400 Bad Request if user ID is invalid', async () => {
+        const invalidUserId = 'invalidUserId';
+
+        const response = await request(app)
+            .delete(`/user/deleteUser/${invalidUserId}`)
+            .expect(400);
+
+        expect(response.body).toHaveProperty('error', 'Invalid user ID');
+    });
+});
---
+++ server/app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+const passport = require('passport');
+const cookieparser = require('cookie-parser');
+const session = require('express-session');
+const path = require('path');
+require('dotenv').config();
+
+const routes = require('./src/Routes/main.routes');
+const lu_routes = require('./src/Routes/learningUnits.routes');
+const user_routes = require('./src/Routes/authentification.routes');
+const fileUploaded = require('./src/Routes/fileUpload');
+const Langchain = require('./src/Routes/Langchain.routes');
+const ChatCompletion = require('./src/Routes/chatCompletion.routes');
+const scheduleAPODEmails = require('./src/utils/scheduleCronjob');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname + '/src', 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(cors({
+  credentials: true,
+  origin: process.env.CORS_ORIGIN_PROD 
+}));
+app.use(cookieparser());
+
+app.use('/data', routes);
+app.use('/user', user_routes);
+app.use('/upload', fileUploaded);
+app.use('/learning', lu_routes);
+app.use('/genai', Langchain);
+app.use('/chat-autocomplete', ChatCompletion);
+
+scheduleAPODEmails();
+
+module.exports = app;
